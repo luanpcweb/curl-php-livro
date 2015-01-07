22221f04d42cf65849c5d58f05f3c9fa5ed171c7
BytesLivres <luan@byteslivres.com.br>
2015-01-07 19:15:28 -0300

    Criado arquivocurl.php

---
+++ curl.php
@@ -0,0 +1,11 @@
+<?php
+
+$url = "http://requestb.in/1mxd5oo1";
+$data = array("nome" => "Joaozinho", "email" => "Joaozinho@example.com");
+
+$request = new HttpRequest($url, HTTP_METHOD_POST);
+$request->setPostFields($data);
+$request->setHeaders(array("Content-Type" => "application/json" ));
+
+$request->send();
+$result = $request->getResponseBody(); 
